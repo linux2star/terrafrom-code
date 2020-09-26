568cbc2a061d8e36f8f6d98ef44c8d58d1d82786
deepak <abc@gmail.com>
2020-09-26 15:30:31 +0000

    new5

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
 provider "aws" {
+  access_key = "AKIAYCVEJYWDIMFBIWWG"
+  secret_key = "GciPOxm5dMg/8lMZzbTLBDI+eDIIXuPhFmaZT471"
+  region     = "us-east-1"
 }
 resource "aws_instance" "example" {
   ami           = "ami-0761dd91277e34178"
   instance_type = "t2.micro"
 }
-#provider "aws" {
-#  access_key = "AKIAYCVEJYWDIMFBIWWG"
-#  secret_key = "GciPOxm5dMg/8lMZzbTLBDI+eDIIXuPhFmaZT471"
-#  region     = "us-east-1"
-#}
-#resource "aws_instance" "example" {
-#  ami           = "ami-0761dd91277e34178"
-#  instance_type = "t2.micro"
-#}
 
