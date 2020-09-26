c59a885419b3585af7dde3a7fb59b12c23ff5fa7
deepak <abc@gmail.com>
2020-09-26 15:29:05 +0000

    new4

--- main.tf
+++ main.tf
@@ -1,19 +1,16 @@
-#provider "aws" {
-#  region  = "us-east-1"
-#  AWS_ACCESS_KEY_ID=AKIAYCVEJYWDIMFBIWWG
-#  AWS_SECRET_ACCESS_KEY=GciPOxm5dMg/8lMZzbTLBDI+eDIIXuPhFmaZT471
-#}
-#resource "aws_instance" "example" {
-#  ami           = "ami-0761dd91277e34178"
-#  instance_type = "t2.micro"
-#}
 provider "aws" {
-  access_key = "AKIAYCVEJYWDIMFBIWWG"
-  secret_key = "GciPOxm5dMg/8lMZzbTLBDI+eDIIXuPhFmaZT471"
-  region     = "us-east-1"
 }
 resource "aws_instance" "example" {
   ami           = "ami-0761dd91277e34178"
   instance_type = "t2.micro"
 }
+#provider "aws" {
+#  access_key = "AKIAYCVEJYWDIMFBIWWG"
+#  secret_key = "GciPOxm5dMg/8lMZzbTLBDI+eDIIXuPhFmaZT471"
+#  region     = "us-east-1"
+#}
+#resource "aws_instance" "example" {
+#  ami           = "ami-0761dd91277e34178"
+#  instance_type = "t2.micro"
+#}
 
