0a200677725e49ee6705311df4c01e41dead8061
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-25 09:53:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/*provider "aws" {
   access_key = "AKIAVAAXINPVM6YF237V"
   secret_key = "U6zCRukJrBr7EK3otNN/j7dgU817jfhlKMOr3r3e"
   region     = "us-east-1"
@@ -6,5 +6,10 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0761dd91277e34178"
   instance_type = "t2.micro"
+}*/
+  
+provider "google" {
+  project = "mission-gcp-certification"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
-
