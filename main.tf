1d9a2d73c79ed46fd53d5ac33a99a80a84172549
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-28 09:44:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ resource "google_compute_network" "main" {
 resource "google_compute_subnetwork" "public" {
   name          = "public"
   ip_cidr_range = "10.0.0.0/24"
-  region        = "us-central1-a"
+  region        = "us-central1"
   network       = google_compute_network.main.id
 }
 # Private Subnet
 resource "google_compute_subnetwork" "private" {
   name          = "private"
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1-a"
+  region        = "us-central1"
   network       = google_compute_network.main.id
 }
 resource "google_compute_instance" "demo" {
