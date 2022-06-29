419972a5893b257923482a9fdff64533dc784587
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-29 10:59:33 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,19 @@
 resource "google_compute_network" "main" {
-  name                    = "main"
+  name                    = var.vap_name
   auto_create_subnetworks = false
   routing_mode = "GLOBAL"
 }
 # Public Subnet
 resource "google_compute_subnetwork" "public" {
   name          = "public"
-  ip_cidr_range = "10.0.0.0/24"
+  ip_cidr_range = var.public_subnet_rang
   region        = "us-central1"
   network       = google_compute_network.main.id
 }
 # Private Subnet
 resource "google_compute_subnetwork" "private" {
   name          = "private"
-  ip_cidr_range = "10.0.1.0/24"
+  ip_cidr_range = var.private_subnet_rang
   region        = "us-central1"
   network       = google_compute_network.main.id
 }
