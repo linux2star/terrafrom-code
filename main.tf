54a96740d485e55066fc2f1d2e29a8d760974422
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-29 08:16:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,20 +24,3 @@ resource "google_compute_subnetwork" "private" {
   region        = "us-central1"
   network       = google_compute_network.main.id
 }
-/*
-resource "google_compute_instance" "demo" {
-  name         = "instance-by-terraform-new"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-  tags = ["terraform", "gcp"]
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  } 
-network_interface {
-    network =  "${google_compute_network.main.name}"
-    subnetwork = "${google_compute_subnetwork.public.name}"
-    access_config {}
- } 
-}*/
