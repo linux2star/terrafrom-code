e3cdf0aed6cea9ba5854eb430752f95b090d511d
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-29 09:58:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,22 @@
 description = "GCS Bucket name. Value should be unique ."
 type        = string
 }
+*/
 
 variable "region" {
 description = "Google Cloud region"
-type        = string
+type        = list(string)
 default     = "us-central1"
 }
-*/
+variable "zone" {
+description = "Google Cloud region"
+type        = list(string)
+default     = "us-central1-a"
+}
+variable "project_name" {
+description = "Google Cloud region"
+type        = list(string)
+default     = "new-project"
+}
+
+
