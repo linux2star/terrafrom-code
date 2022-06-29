28522892299bb4ace8b9ef80f67113ad6e1db80e
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-29 11:00:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,5 +19,18 @@ description = "Google Cloud region"
 type        =  string
 default     = "new-project"
 }
+variable "vpc_name" {
+description = "Google Cloud region"
+type        =  string
+default     = "new-vpc"
+}
+variable "public_subnet_rang" {
+description = "Google Cloud region"
+type        =  string
+}
+variable "private_subnet_rang" {
+description = "Google Cloud region"
+type        =  string
+}
 
 
