66950f8a0551952014f5d72ec38c7e537b1469a2
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-29 11:31:25 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "demo" {
   name         = "${var.environment}-instance"
   machine_type = var.machine_type
-  zone         = var.region
+  zone         = var.zone
   tags = ["terraform", "gcp"]
   boot_disk {
     initialize_params {
