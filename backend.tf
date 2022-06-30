8f5a41f9c465d8c73ec90f5edbcb816ed2d2ad6d
Your Name <you@example.com>
2022-06-30 09:25:50 +0530

    adding uat env for testing

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "gcs" {
     bucket  = "bucket-for-statefile-48693"
-    #prefix  = "terraform/uat-state-file/uat"
-    prefix  = "terraform/dev-state-file/dev"
+    prefix  = "terraform/uat-state-file/uat"
+    #prefix  = "terraform/dev-state-file/dev"
     credentials = "keys.json"
   }
 }
