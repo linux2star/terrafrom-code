c992148820608ab83a6c50e618645fc358667162
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-30 08:07:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-/*variable "bucket_name" {
-description = "GCS Bucket name. Value should be unique ."
-type        = string
-}
-*/
-
 variable "region" {
 description = "Google Cloud region"
 type        =  string
