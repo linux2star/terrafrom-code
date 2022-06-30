5be3cc32d01687d269973c857ffb95dcbc454209
linux2star <69943949+linux2star@users.noreply.github.com>
2022-06-30 08:13:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "gcs" {
     bucket  = "bucket-for-statefile-48693"
-    prefix  = "terraform/state"
+    #prefix  = "terraform/state"
+    prefix  = "terraform/${var.environment}-state-file"
     credentials = "keys.json"
   }
 }
