723833fffd9987ff005ae5a5d1e3462323395728
linux2star <69943949+linux2star@users.noreply.github.com>
2022-07-04 10:12:13 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     #bucket  = "bucket-for-statefile-48693"
     #prefix  = "terraform/uat-state-file/uat"
     #prefix  = "terraform/dev-state-file/dev"
-    credentials = "keys.json"
+    #credentials = "keys.json"
   }
 }
 
